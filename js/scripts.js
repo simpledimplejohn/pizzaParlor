d4afb00675f27bd16c63a8056a553b523708a355
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-18 13:12:25 -0700

    add topings branching to scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -4,30 +4,43 @@ function Pizza(size, topping) {
   this.price = 0
 }
 
-Pizza.protototype.pizzaPrice = function() {
+Pizza.prototype.pizzaPrice = function() {
   let size = this.size
   let topping = this.topping
+
   if (size === 1) {
     console.log("1 works")
-
+    this.price += 10;
   } else if (size === 2) {
     console.log("2 works")
-  } else {
+    this.price += 20;
+  } else if (size === 3) {
     console.log("3 works")
+    this.price += 30
+  } 
+  if (topping === 1) {
+    console.log("topping works")
+    this.price *= 1.8;
+  } else if (topping === 2) {
+    console.log("toppings 2 works")
+    this.price *= 1.2;
   }
+  return this.price
 }
 
+
 $(document).ready(function() {
-  $("#movie-form").submit(function(event) {
+  $("#order-form").submit(function(event) {
     event.preventDefault();
+    console.log("document.ready works!")
     const sizeInput = parseInt($("#size").val());
-    const toppingInput = parseInt($("#size").val());
+    const toppingInput = parseInt($("#toppings").val());
 
     let newPizza = new Pizza(sizeInput, toppingInput);
 
     newPizza.pizzaPrice();
     console.log(newPizza.price)
-
+    $("#result").text(newPizza.price)
 
   })
 })
