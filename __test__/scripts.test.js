ecd2b917b558a54b4642af77f738234b18626534
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-26 17:52:20 -0700

    pass first test

---
+++ __test__/scripts.test.js
@@ -0,0 +1,7 @@
+import Pizza from './../src/js/scripts.js';
+
+describe('Pizza', () => {
+  test('should create an object with three properties', () => {
+    const newPizza = new Pizza(1, 1)
+  })
+})
--- src/js/scripts.js
+++ src/js/scripts.js
@@ -1,4 +1,3 @@
-console.log("scripts works!");
 
 
 export default class Pizza {
@@ -7,16 +6,4 @@ export default class Pizza {
     this.topping = topping;
     this.price = 0;
   }
-  pizzaPrice () {
-    if (this.topping === 1) {
-      this.price += (5 + this.size);
-    } else if (this.topping === 2) {
-      this.price += (6 + this.size);
-    } else if (this.topping === 3) {
-      this.price += (4 + this.size);
-    } else {
-      this.price += (3 + this.size);
-    }
-  }
 }
-
