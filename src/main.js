2e2de276570cf8f95323ac3f79a1e062ceb4bc94
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-26 17:06:34 -0700

    fix size error in scripts.js

--- src/main.js
+++ src/main.js
@@ -4,17 +4,23 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import './css/styles.css';
 import Pizza from './js/scripts.js';
 
+
+
+
+
+
 $(document).ready(function() {
 
   $("#order-form").submit(function(event) {
     event.preventDefault();
-    console.log("document.ready works!")
+    console.log("document.ready works!");
     const sizeInput = parseInt($("#size").val());
     const toppingInput = parseInt($("#toppings").val());
 
     let newPizza = new Pizza(sizeInput, toppingInput);
     newPizza.pizzaPrice();
-    console.log(newPizza.price)
-    $("#result").text(newPizza.price)
+    console.log(newPizza.price);
+    $("#result").text(newPizza.price);
   })
 })
+
