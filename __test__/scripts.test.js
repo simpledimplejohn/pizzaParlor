df4dcac0f717e1a22e3f75493696429edabc4b13
Simple Dimple John <simpledimplejohn@gmail.com>
2021-06-26 18:12:37 -0700

    pass second test, create  newPizza

--- __test__/scripts.test.js
+++ __test__/scripts.test.js
@@ -1,7 +1,19 @@
+import expectationResultFactory from 'jest-jasmine2/build/expectationResultFactory';
 import Pizza from './../src/js/scripts.js';
 
 describe('Pizza', () => {
   test('should check that Pizza class is present', () => {
     const newPizza = new Pizza()
-  })
-})
+  });
+});
+
+describe('Pizza', () => {
+  test('should create a pizza with size, topping, and price properties', () => {
+    const newPizza = new Pizza(10,1)
+    expect(newPizza).toEqual({
+      size: 10,
+      topping: 1,
+      price: 0,
+    })
+  });
+});
